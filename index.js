6141626b84eff042fb1d776af3621b4eb46114d6
iCode-Cat <74076633+iCode-Cat@users.noreply.github.com>
2020-12-14 23:21:33 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,25 +1,39 @@
-
 //Vars
-let value = -48;
+let value = 0;
 let count = 0;
-let loop = 0;
+let loop = 1;
 let timer = 59;
+let mistake = 1;
 let stopTimer = true;
 let array;
+let scores = 1;
 //DOM
 const start = document.querySelector('.start');
 const sentence = document.querySelector('#sentence');
 const seconds = document.querySelector('.seconds');
 const res = document.querySelector('.result');
+const change = document.querySelector('.change-text');
+const changeClick = document.querySelector('.changeButton');
+const custom = document.querySelector('.custom');
+const textarea = document.querySelector('textarea');
+const letter = document.querySelector('.letters');
+const errors = document.querySelector('.mistakes');
+const scoreSystem = document.querySelector('.scores');
 
-array = [sentence.innerText.split("")];
+
+
+let text = "lorem ipsum dolor sit amet consectetur adipisicing elit. Illum atque quod ullam aperiam alias consequuntur illo nulla sequi dolor fugiat. Laudantium consequuntur dolorum magnam ipsum fuga vitae maiores eaque quasi?Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, nesciunt eveniet. Minima, magnam optio deleniti impedit odio commodi facilis rerum quisquam illum quos eos culpa at doloribus molestias corporis dolore"
+
+
 
 //Fire events after global
-this.addEventListener('keypress' , (e)=> {
-    if(stopTimer) {
+this.addEventListener('keypress', (e) => {
+    array = [text.split("")];
+    if (stopTimer) {
         countTimer()
     }
     letterCheck(e)
+
 })
 
 
@@ -29,22 +43,24 @@ function letterCheck(e) {
     stopTimer = false;
     start.style.opacity = 0;
     //If the pressed key true, highlight
-    if(e.key === array[0][count++]) {
-        changeColor(array[0][count-1])
-        if(value > 0) {
-            sentence.style.marginRight = value++ + `${'%'}`;
-            
+    if (e.key === array[0][count++]) {
+        scoreSystem.innerText = scores++*10
+        const span = document.querySelectorAll('span');
+        span[count - 1].style.color = "#2e86de"
+        span[count].style.color = "red"
+        if(e.key != " ") {
+            letter.innerText = loop++
         }
-        else {
-            sentence.style.marginRight = value++ + `${'%'}`;
-        }
-    }else {
+
+    } else {
         count--
+        errors.innerText = mistake++
+        scoreSystem.innerText = scores--*10
     }
 }
 //Function of count timer
 function countTimer() {
-    
+
     setInterval(() => {
         seconds.innerText = timer--
 
@@ -53,11 +69,48 @@ function countTimer() {
 }
 
 
+
 //Factory
-function changeColor(letter) {   
- 
-    console.log(letter);
-    sentence.innerHTML = sentence.innerHTML.replace(letter , `<span style='color:red;'>${letter}</span>`)
+function changeColor(text) {
+
+    text.split("").forEach(letter => {
+        const characterSpan = document.createElement('span');
+        characterSpan.innerText = letter;
+        if (characterSpan.innerText === " ") {
+            characterSpan.style.marginRight = "5px"
+        }
+        sentence.appendChild(characterSpan);
+        console.log();
+    })
+
+    console.log(sentence.innerText);
+
     // sentence.innerHTML = sentence.innerHTML.replace(arrayLetter , `<span style="color:red;"> ${arrayLetter} </span>`)
 
+}
+
+change.onclick = (e) => {
+    changetext()
+}
+
+function changetext(e) {
+    custom.style.display = "flex"
+}
+
+
+//Close the window
+this.onclick = (e) => {
+    console.log(e.target);
+    if (e.target === document.querySelector('.custom')) {
+        custom.style.display = "none";
+
+
+    }
+}
+changeColor(text)
+
+changeClick.onclick = () => {
+    text = textarea.value;
+    changeColor(text)
+    console.log(textarea.value);
 }
